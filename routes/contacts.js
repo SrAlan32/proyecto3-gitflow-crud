9c83f0156ce41e1f20422d2092045d45f1825427
SrAlan32 <alan.bertrand52@gmail.com>
2026-07-31 23:04:01 -0400

    feat: add update and delete contact endpoints (PUT, DELETE /contacts/:id)

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createContact, getAllContacts, getContactById } = require('../controllers/contactsController');
+const {
+  createContact, getAllContacts, getContactById, updateContact, deleteContact
+} = require('../controllers/contactsController');
 
 router.post('/', createContact);
 router.get('/', getAllContacts);
 router.get('/:id', getContactById);
+router.put('/:id', updateContact);
+router.delete('/:id', deleteContact);
 
 module.exports = router;
